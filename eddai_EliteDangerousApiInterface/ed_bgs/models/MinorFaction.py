e8fb2380c6eb22a51433d69475df7d07a63faa02
fabio zorzetto <fabio.zorzetto.01@gmail.com>
2022-08-22 20:17:28 +0200

    Update MinorFaction.py

--- eddai_EliteDangerousApiInterface/ed_bgs/models/MinorFaction.py
+++ eddai_EliteDangerousApiInterface/ed_bgs/models/MinorFaction.py
@@ -1,4 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class MinorFaction(models.Model):
-    pass
+    """
+    
+    """
+    name = models.CharField(
+        max_length=255, unique=True, verbose_name=_('name')
+    )
+    allegiance = None
+    government = None
+    
+    
+    updated = models.DateTimeField(
+        auto_now=True
+    )
