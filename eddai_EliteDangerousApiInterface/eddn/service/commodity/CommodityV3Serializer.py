1e36640c33f2b4a28e88fedf1db38e5373d07cf2
fabio zorzetto <fabio.zorzetto.01@gmail.com>
2023-04-09 18:01:18 +0200

    Update CommodityV3Serializer.py
    add CommoditySerializer

--- eddai_EliteDangerousApiInterface/eddn/service/commodity/CommodityV3Serializer.py
+++ eddai_EliteDangerousApiInterface/eddn/service/commodity/CommodityV3Serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from eddn.service.seriallizers.BaseSerializer import BaseSerializer
+from eddn.service.commodity.CommoditySerializer import CommoditySerializer
 
 from core.utility import update_or_create_if_time
 
@@ -14,7 +15,10 @@ class CommodityV3Serializer(BaseSerializer):
     stationName = serializers.CharField(
         min_length=1
     )
-    commodities = None
+    commodities = serializers.ListField(
+        child=CommoditySerializer(),
+        min_length=1
+    )
     timestamp = serializers.DateTimeField(
         input_formats=[
             "%Y-%m-%dT%H:%M:%SZ"
