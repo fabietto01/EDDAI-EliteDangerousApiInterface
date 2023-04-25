d688212ba573895c69e7461e1aac3958381c2f50
Fabio Zorzetto <fabio.zorzetto.01@gmail.com>
2023-04-25 14:32:09 +0200

    edit settings

--- eddai_EliteDangerousApiInterface/eddai_EliteDangerousApiInterface/settings/default.py
+++ eddai_EliteDangerousApiInterface/eddai_EliteDangerousApiInterface/settings/default.py
@@ -167,7 +167,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100000,
+    'PAGE_SIZE': 500,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
