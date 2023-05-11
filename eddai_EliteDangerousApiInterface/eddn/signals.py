036d71410c03d6355529b12b0b31079fe1c8f522
fabio zorzetto <fabio.zorzetto.01@gmail.com>
2023-05-12 00:04:49 +0200

    celey funziona coretamente

--- eddai_EliteDangerousApiInterface/eddn/signals.py
+++ eddai_EliteDangerousApiInterface/eddn/signals.py
@@ -1,8 +1,9 @@
 from celery.signals import worker_ready
+from celery.worker.consumer import Consumer
 from eddn.service.client import EddnClient
 
 @worker_ready.connect
-def at_start(sender, **kwargs):
-    if "workerEDDN" in sender:
+def at_start(sender:Consumer, **kwargs):
+    if str(sender.hostname).startswith('workerEDDN'):
         with sender.app.connection() as conn:
             sender.app.send_task("eddn.tasks.eddn", connection=conn)
