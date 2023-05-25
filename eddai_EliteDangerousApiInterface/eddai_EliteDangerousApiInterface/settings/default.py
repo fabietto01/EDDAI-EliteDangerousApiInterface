72bf17898d8779eeca520981d9f0c394b5e33791
fabio zorzetto <fabio.zorzetto.01@gmail.com>
2023-05-25 21:42:46 +0200

    edit

--- eddai_EliteDangerousApiInterface/eddai_EliteDangerousApiInterface/settings/default.py
+++ eddai_EliteDangerousApiInterface/eddai_EliteDangerousApiInterface/settings/default.py
@@ -99,12 +99,12 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
-    'ed_info':{
+    'default':{
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ed_info',
         'USER': 'ed_info-user',
-        'PASSWORD': os.environ.get('DJANGO_DATABASES_ED_PASSWORD'),
-        'HOST': os.environ.get('DJANGO_DATABASES_ED_HOST', 'localhost'),
+        'PASSWORD': os.environ.get('MYSQL_USER_ED_INFO_PASSWORD'),
+        'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
         'PORT': '3306',
     }
 }
