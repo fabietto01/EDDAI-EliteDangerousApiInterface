fef0693dc47d5944d17dbb152ad50c43b79f33e4
fabio zorzetto <fabio.zorzetto.01@gmail.com>
2024-03-24 15:37:11 +0100

    StationModelSerializes edit

--- eddai_EliteDangerousApiInterface/ed_station/api/serializers/StationSerializes.py
+++ eddai_EliteDangerousApiInterface/ed_station/api/serializers/StationSerializes.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
+from django.db import OperationalError, ProgrammingError
+import uuid
 
 from ed_station.api.serializers import NestedServiceModelSerializes
 
 from ed_station.models import Station, Service
+from ed_economy.models import Economy
+
+
+from core.api.fields import CacheChoiceField
+from core.utility import get_values_list_or_default
 
 class StationModelSerializes(serializers.ModelSerializer):
     """
