d9b14cc2d70fad89b703df9a306f4a3c072f74a4
fabio zorzetto <fabio.zorzetto.01@gmail.com>
2024-07-06 12:59:23 +0200

    Update admin.py

--- eddai_EliteDangerousApiInterface/eddn/admin.py
+++ eddai_EliteDangerousApiInterface/eddn/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from eddn.models import DataLog
+
+@admin.register(DataLog)
+class DataLogModelAdmin(admin.ModelAdmin):
+    model = DataLog
+    search_fields = ("schema","error")
+    list_display = ("schema", "error", "update", "creat_at")
+    list_display_links = ("schema",)
+    readonly_fields = ("creat_at", "update", "error")
+    list_filter = ("schema","error")
+    fieldsets = [
+        (None, {
+            "fields": (
+                "schema",
+                "data",
+                "error",
+            )
+        }),
+        ("Date", {
+            "fields": ("creat_at", "update")
+        })
+    ]
