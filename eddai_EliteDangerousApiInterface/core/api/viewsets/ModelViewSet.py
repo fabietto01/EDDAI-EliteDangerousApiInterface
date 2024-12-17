f7f5053a1c464138260367a7af1a1fe33b6cc9b0
fabio zorzetto <fabio.zorzetto.01@gmail.com>
2024-12-17 18:19:34 +0000

    Aggiungi OwnerAndDateModelViewSet per gestire i campi created_by e updated_by

---
+++ eddai_EliteDangerousApiInterface/core/api/viewsets/ModelViewSet.py
@@ -0,0 +1,27 @@
+from core.models import OwnerAndDateModels
+from rest_framework.viewsets import ModelViewSet
+
+class OwnerAndDateModelViewSet(ModelViewSet):
+    """
+    OwnerAndDateModelViewSet is a custom viewset that extends the ModelViewSet class.
+    It overrides the perform_create and perform_update methods to automatically set
+    the created_by and updated_by fields to the current user.
+    Methods:
+        perform_create(serializer: OwnerAndDateModels):
+            Saves the serializer with the created_by and updated_by fields set to the current user.
+        perform_update(serializer: OwnerAndDateModels):
+            Saves the serializer with the updated_by field set to the current user.
+    """
+    
+    def perform_create(self, serializer:OwnerAndDateModels):
+        user = self.request.user
+        serializer.save(
+            created_by=user,
+            updated_by=user
+        )
+    
+    def perform_update(self, serializer:OwnerAndDateModels):
+        user = self.request.user
+        serializer.save(
+            updated_by=user
+        )
