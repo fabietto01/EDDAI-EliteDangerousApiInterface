0917c0a7a5be29c8503cd379317e15c9db6fe04f
fabio zorzetto <fabio.zorzetto.01@gmail.com>
2025-01-06 22:42:45 +0100

    Correggi il percorso del Dockerfile nello script di deploy e aggiungi la password per Redis nei servizi di docker-compose

--- deply.sh
+++ deply.sh
@@ -4,7 +4,7 @@
 git pull --rebase origin main
 
 # Rebuild the Docker image
-docker build --file eddai_EliteDangerousApiInterface\Dockerfile -t eddaielitedangerousapiinterface:latest "eddai_EliteDangerousApiInterface"
+docker build --file eddai_EliteDangerousApiInterface/Dockerfile -t eddaielitedangerousapiinterface:latest "eddai_EliteDangerousApiInterface"
 
 # Apply the latest migrations
 docker run --env-file .env --rm eddaielitedangerousapiinterface:latest python manage.py migrate
