5658a0e229228521e36635139dfee0e9439ed461
fabio zorzetto <fabio.zorzetto.01@gmail.com>
2025-01-06 15:10:53 +0100

    Aggiungi configurazione di test per Django e modifica il workflow di test per includere il servizio PostGIS

---
+++ eddai_EliteDangerousApiInterface/eddai_EliteDangerousApiInterface/settings/test.py
@@ -0,0 +1,21 @@
+from .default import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+# Un elenco di tutte le persone che ricevono notifiche di errore del codice. 
+# https://docs.djangoproject.com/en/4.1/ref/settings/#admins
+ADMINS = [("Fabio Zorzetto", "fabio.zorzetto.01@gmail.com")]
+
+# Caches
+# https://docs.djangoproject.com/en/5.1/ref/settings/#caches
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "unique-snowflake",
+    }
+}
+
+CELERY_BROKER_URL = 'memory://'
+CELERY_RESULT_BACKEND = "cache+memory://"
