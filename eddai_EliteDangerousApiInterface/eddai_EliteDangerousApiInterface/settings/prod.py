2e1e2a3ff2695e7937ce095075e6aaf9b00aafe8
fabio zorzetto <fabio.zorzetto.01@gmail.com>
2025-01-14 16:51:49 +0000

    Rimuovi righe vuote nel file di configurazione prod.py

--- eddai_EliteDangerousApiInterface/eddai_EliteDangerousApiInterface/settings/prod.py
+++ eddai_EliteDangerousApiInterface/eddai_EliteDangerousApiInterface/settings/prod.py
@@ -5,8 +5,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-
-
 # Un elenco di tutte le persone che ricevono notifiche di errore del codice. 
 # https://docs.djangoproject.com/en/4.1/ref/settings/#admins
 ADMINS = [("Fabio Zorzetto", "fabio.zorzetto.01@gmail.com")]
