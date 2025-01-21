f04f679359bc3be5e4095527fbcace0fa030e389
fabio zorzetto <fabio.zorzetto.01@gmail.com>
2025-01-21 22:05:15 +0100

    Aggiungi la logica per rimuovere il filtro 'order_by_distance_st' se 'order_by_system' non è presente

--- eddai_EliteDangerousApiInterface/ed_body/api/filterSet/baseBodyFilterSet.py
+++ eddai_EliteDangerousApiInterface/ed_body/api/filterSet/baseBodyFilterSet.py
@@ -6,8 +6,12 @@ from ed_core.functions import Distanza3D
 from ed_body.models import BaseBody
 from ed_system.models import System
 
-
 class BaseBodyFilterSet(django_filters.FilterSet):
+
+    def __init__(self, param=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if not self.data.get('order_by_system'):
+            self.filters.pop('order_by_distance_st', None)
 
     @staticmethod
     def filter_by_system(queryset, name, value:System):
@@ -16,13 +20,20 @@ class BaseBodyFilterSet(django_filters.FilterSet):
                 F('system__coordinate'),
                 point=value.coordinate
             )
-        ).order_by('distance_st')
+        )
     
     order_by_system = django_filters.ModelChoiceFilter(
         queryset=System.objects.all(),
         method='filter_by_system',
         label='from system',
         distinct=True
+    )
+
+    order_by_distance_st = django_filters.OrderingFilter(
+        fields=(
+            ('distance_st', 'distance_st'),
+        ),
+        initial='distance_st'
     )
 
     class Meta:
