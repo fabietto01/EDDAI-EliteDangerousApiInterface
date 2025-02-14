4af261d3f25e7854ab0124df64bb2407cfc21b7d
fabio zorzetto <fabio.zorzetto.01@gmail.com>
2025-02-14 16:01:58 +0000

    Aggiunta di una nuova migrazione per modificare le opzioni dei modelli di atmosfera, pianeta e stella, inclusi aggiornamenti ai campi per la validazione e l'ordinamento.

--- eddai_EliteDangerousApiInterface/ed_body/models/Star.py
+++ eddai_EliteDangerousApiInterface/ed_body/models/Star.py
@@ -11,40 +11,46 @@ class Star(BaseBody):
         verbose_name=_('absolute magnitude'),
         validators=[
             MinValueValidator(0, _('the absolute magnitude cannot be less than 0'))
-        ]
+        ],
+        null=True, blank=True,
     )
     age = models.FloatField(
         verbose_name=_('age'),
         help_text=_('age in millions of years'),
         validators=[
             MinValueValidator(0, _('the age cannot be less than 0'))
-        ]
+        ],
+        null=True, blank=True,
     )
     luminosity = models.ForeignKey(
         StarLuminosity, models.PROTECT,
         verbose_name=_('luminosity'),
         related_name='%(app_label)s_%(class)s_related',
-        related_query_name='%(app_label)s_%(class)ss'
+        related_query_name='%(app_label)s_%(class)ss',
+        null=True, blank=True,
     )
     starType = models.ForeignKey(
         StarType, models.PROTECT,
         verbose_name=_('star type'),
         related_name='%(app_label)s_%(class)s_related',
-        related_query_name='%(app_label)s_%(class)ss'
+        related_query_name='%(app_label)s_%(class)ss',
+        null=True, blank=True,
     )
     stellarMass = models.FloatField(
         verbose_name=_('stellar mass'),
         help_text=_("mass as multiple of Sol's mass"),
         validators=[
             MinValueValidator(0, _('the stellar mass cannot be less than 0'))
-        ]
+        ],
+        null=True, blank=True,
     )
     subclass = models.IntegerField(
         verbose_name=_('subclass'),
         validators=[
             MinValueValidator(0, _('the subclass cannot be less than 0')),
             MaxValueValidator(9, _('the subclass cannot be greater than 9'))
-        ]
+        ],
+        null=True, blank=True,
     )
 
     class Meta:
