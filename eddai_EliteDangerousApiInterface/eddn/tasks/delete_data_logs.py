6b7d20193b1f492a05120d2b302a36cd6d6096ec
fabio zorzetto <fabio.zorzetto.01@gmail.com>
2025-02-23 12:31:25 +0000

    Rimozione variabile non necessaria nella funzione di eliminazione dei log dei dati

--- eddai_EliteDangerousApiInterface/eddn/tasks/delete_data_logs.py
+++ eddai_EliteDangerousApiInterface/eddn/tasks/delete_data_logs.py
@@ -4,4 +4,4 @@ from eddn.models import DataLog
 
 @shared_task(name='delete_data_logs', ignore_result=True)
 def delete_data_logs(max_count):
-    data_logs = DataLog.objects.filter(_count__gte=max_count).delete()
+    DataLog.objects.filter(_count__gte=max_count).delete()
