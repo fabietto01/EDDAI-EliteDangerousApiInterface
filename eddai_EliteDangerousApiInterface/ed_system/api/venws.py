8a96aca77698fcf526271280c66bb40daebf5f1d
fabio zorzetto <fabio.zorzetto.01@gmail.com>
2025-03-07 16:27:49 +0000

    Aggiunta documentazione per l'API nel SystemViewSet e aggiornato il campo di ricerca per includere l'indirizzo.

--- eddai_EliteDangerousApiInterface/ed_system/api/venws.py
+++ eddai_EliteDangerousApiInterface/ed_system/api/venws.py
@@ -9,9 +9,19 @@ from .filters import SystemFilterSet
 
 from ed_system.models import System
 
+from drf_spectacular.utils import extend_schema, extend_schema_view
+
+@extend_schema_view(
+    list=extend_schema(description="Returns a list of systems"),
+    retrieve=extend_schema(description="Returns the details of a system by ID"),
+    create=extend_schema(exclude=True), 
+    update=extend_schema(exclude=True), 
+    partial_update=extend_schema(exclude=True), 
+    destroy=extend_schema(exclude=True)
+)
 class SystemViewSet(DistanceModelMixin, OwnerAndDateModelViewSet):
     """
-    
+    Handles requests related to systems.
     """
     queryset = System.objects.all()
     serializer_class = SystemSerializer
@@ -19,4 +29,4 @@ class SystemViewSet(DistanceModelMixin, OwnerAndDateModelViewSet):
     filter_param_distance = 'distance_by_system'
     filterset_class = SystemFilterSet
     filter_backends = [SearchFilter, DjangoFilterBackend]
-    search_fields = ['name']
+    search_fields = ['name', 'address']
