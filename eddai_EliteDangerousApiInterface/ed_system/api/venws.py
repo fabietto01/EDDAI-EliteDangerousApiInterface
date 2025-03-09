8a7f4eb0137468b3d85d659e1d7a663eba9455e8
fabio zorzetto <fabio.zorzetto.01@gmail.com>
2025-03-09 01:13:10 +0000

    Aggiunta annotazione del tipo di ritorno al metodo get_distance_st nel DistanceSerializer e aggiornato il messaggio di descrizione per l'endpoint list nel venws.py per includere informazioni sulla distanza tra sistemi.

--- eddai_EliteDangerousApiInterface/ed_system/api/venws.py
+++ eddai_EliteDangerousApiInterface/ed_system/api/venws.py
@@ -12,7 +12,11 @@ from ed_system.models import System
 from drf_spectacular.utils import extend_schema, extend_schema_view
 
 @extend_schema_view(
-    list=extend_schema(description="Returns a list of systems"),
+    list=extend_schema(
+        description="Returns a list of systems, if the parameter distance_by_system is passed,\
+            it returns the distance between systems in the distance_st field",
+        responses={200: SystemDistanceSerializer(many=True)}
+    ),
     retrieve=extend_schema(description="Returns the details of a system by ID"),
     create=extend_schema(exclude=True), 
     update=extend_schema(exclude=True), 
