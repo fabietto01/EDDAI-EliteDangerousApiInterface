076706a85ec57a7cb86f1391f568807dfe431292
fabio zorzetto <fabio.zorzetto.01@gmail.com>
2025-03-19 17:02:25 +0000

    inizio esperimentazione

--- eddai_EliteDangerousApiInterface/core/api/fields/CacheSlugRelatedField.py
+++ eddai_EliteDangerousApiInterface/core/api/fields/CacheSlugRelatedField.py
@@ -1,9 +1,25 @@
 from rest_framework.serializers import SlugRelatedField 
 from django.core.cache import cache
 
+
+from django.db.models import Manager
+from django.db.models.query import QuerySet
+from django.utils.encoding import smart_str, uri_to_iri
+from django.core.exceptions import ImproperlyConfigured, ObjectDoesNotExist
+
 class CacheSlugRelatedField(SlugRelatedField):
-    """
-    DEV
-    capo da implementare con gestione di cache
-    """
-    pass
+    
+    def to_internal_value(self, data):
+
+        cache_key = f"{self.queryset.model._meta.label_lower}:{self.slug_field}:{data}"
+        cached_object = cache.get(cache_key)
+
+
+        queryset = self.get_queryset()
+        try:
+            return queryset.get(**{self.slug_field: data})
+        except ObjectDoesNotExist:
+            self.fail('does_not_exist', slug_name=self.slug_field, value=smart_str(data))
+        except (TypeError, ValueError):
+            self.fail('invalid')
+    
