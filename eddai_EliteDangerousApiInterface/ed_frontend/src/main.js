9c4ce250e58a85595cadcdc30eec3b20988430da
fabio zorzetto <fabio.zorzetto.01@gmail.com>
2025-05-06 13:39:46 +0000

    Rimosso Bootstrap e Bootstrap Icons, aggiornato il CSS e migliorata la struttura dei template per una gestione più efficiente delle dipendenze e del layout.

--- eddai_EliteDangerousApiInterface/ed_frontend/src/main.js
+++ eddai_EliteDangerousApiInterface/ed_frontend/src/main.js
@@ -1,5 +1,3 @@
-import "bootstrap/dist/css/bootstrap.css";
-import 'bootstrap-icons/font/bootstrap-icons.css';
 import './assets/main.css'
 
 import { createApp } from 'vue'
@@ -7,8 +5,6 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
-import "bootstrap/dist/js/bootstrap.js";
 
 app.use(router)
 
