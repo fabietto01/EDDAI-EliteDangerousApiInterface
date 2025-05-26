ef892e34627b3c6a1bf997b70dc863ffbea06ee3
fabio zorzetto <fabio.zorzetto.01@gmail.com>
2025-05-26 13:44:26 +0000

    Rimosso il percorso e il componente per la vista "systems" dal router

--- eddai_EliteDangerousApiInterface/ed_frontend/src/router/index.js
+++ eddai_EliteDangerousApiInterface/ed_frontend/src/router/index.js
@@ -8,11 +8,6 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
-    },
-    {
-      path: '/systems/',
-      name: 'systems',
-      component: () => import('../views/SystemView.vue'),
     }
   ],
 })
