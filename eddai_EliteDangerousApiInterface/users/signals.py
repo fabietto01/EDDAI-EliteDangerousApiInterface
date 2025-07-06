d5bfb132ea031815fb7c4db25f3ace755c87cb27
fabio zorzetto <fabio.zorzetto.01@gmail.com>
2025-07-06 21:07:56 +0000

    Fix update_cmdr_profile to handle exceptions more gracefully during social account updates

--- eddai_EliteDangerousApiInterface/users/signals.py
+++ eddai_EliteDangerousApiInterface/users/signals.py
@@ -3,17 +3,24 @@ from allauth.socialaccount.signals import social_account_updated, social_account
 from .utility import get_cmdr_name
 
 from .models import User
+import logging
+
+log = logging.getLogger('django')
 
 @receiver(social_account_added)
 def update_cmdr_profile(sender, request, sociallogin, **kwargs):
     """
     Aggiorna il profilo CMDR quando un account social viene aggiornato
     """
-    if sociallogin.account.provider == 'frontier':
-        user:User = sociallogin.user
+    try:
+        if sociallogin.account.provider == 'frontier':
+            user:User = sociallogin.user
 
-        username = get_cmdr_name(user)
+            username = get_cmdr_name(user)
 
-        if username and username != user.username:
-            user.username = username
-            user.save()
+            if username and username != user.username:
+                user.username = username
+                user.save()
+    except Exception as e:
+        log.error(f"Error updating CMDR profile for user {user.id}: {e}", exc_info=True)
+        
