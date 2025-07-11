e9947ce4a89b13cdb45f2c3286613208b8555190
fabio zorzetto <fabio.zorzetto.01@gmail.com>
2025-07-11 13:11:38 +0000

    Aggiungi gestore del segnale per l'aggiunta di account social e aggiorna il profilo CMDR

--- eddai_EliteDangerousApiInterface/ed_dbsync/signals.py
+++ eddai_EliteDangerousApiInterface/ed_dbsync/signals.py
@@ -10,83 +10,66 @@ from ed_dbsync.tasks.capiJournalSync import CapiJournalSync
 from django_celery_beat.models import PeriodicTask, IntervalSchedule
 
 log_dja = logging.getLogger("django")
-log_name = logging.getLogger(__name__)
-
-@receiver(social_account_added)
-def update_cmdr_profile(sender, **kwargs):
-    log_dja.info("DEBUG: social_account_added signal received (ed_dbsync.signals)")
-    log_name.info("DEBUG: social_account_added signal received (ed_dbsync.signals)")
 
 @receiver(social_account_updated)
-def update_social_account(sender, **kwargs):
-    log_dja.info("DEBUG: social_account_updated signal received (ed_dbsync.signals)")
-    log_name.info("DEBUG: social_account_updated signal received (ed_dbsync.signals)")
+def update_social_account(sender, request, sociallogin:SocialLogin, **kwargs):
+    """
+    Signal handler for when a social account is updated.
+    This can be used to refresh the user's CAPI token or perform other actions.
+
+    Args:
+        sender: The sender of the signal.
+        request: The HTTP request object.
+        sociallogin: The social login instance containing the updated account information.
+        **kwargs: Additional keyword arguments.
+    """
+    log_dja.info(f"Social account updated for user: {sociallogin.user.username} (ID: {sociallogin.user.id}). Provider {sociallogin.provider} ", exc_info={"sociallogin": sociallogin})
+    if sociallogin.provider != 'frontier':
+        log_dja.info(f"Social account updated for non-frontier provider: {sociallogin.provider}")
+        return
+    
+    user:User = sociallogin.user
+    log_dja.info(f"Social account updated for user: {user.username}")
+    interval_schedule, create = IntervalSchedule.objects.get_or_create(
+        every=24,  # 24 hours
+        period=IntervalSchedule.HOURS,
+    )
+    defaults={
+        'task': CapiJournalSync.name,
+        'interval': interval_schedule,
+        'enabled': True,
+        'one_off': False,
+        'queue': 'ed_dbsync',
+        'args': [],
+        'kwargs': str({'user_id': f'{user.id}'}).replace("'", '"'),
+        'description': f"Sync CAPI journal for user {user.username} (ID: {user.id})",
+    }
+    PeriodicTask.objects.update_or_create(
+        name=CapiJournalSync.get_task_name_for_periodic_task(user),
+        defaults=defaults
+    )
+    log_dja.info(f"Periodic task for CAPI journal sync created/updated for user: {user.username} (ID: {user.id})")
 
 @receiver(social_account_removed)
-def remove_social_account(sender, **kwargs):
-    log_dja.info("DEBUG: social_account_removed signal received (ed_dbsync.signals)")
-    log_name.info("DEBUG: social_account_removed signal received (ed_dbsync.signals)")
+def remove_social_account(sender, request, socialaccount:SocialAccount, **kwargs):
+    """
+    Signal handler for when a social account is removed.
+    This can be used to stop the user's CAPI journal sync task.
 
-# @receiver(social_account_updated)
-# def update_social_account(sender, request, sociallogin:SocialLogin, **kwargs):
-#     """
-#     Signal handler for when a social account is updated.
-#     This can be used to refresh the user's CAPI token or perform other actions.
-
-#     Args:
-#         sender: The sender of the signal.
-#         request: The HTTP request object.
-#         sociallogin: The social login instance containing the updated account information.
-#         **kwargs: Additional keyword arguments.
-#     """
-#     log_dja.info(f"Social account updated for user: {sociallogin.user.username} (ID: {sociallogin.user.id}). Provider {sociallogin.provider} ", exc_info={"sociallogin": sociallogin})
-#     print(f"Social account updated for user: {sociallogin.user.username} (ID: {sociallogin.user.id}). Provider {sociallogin.provider} ")
-#     if sociallogin.provider != 'frontier':
-#         log_dja.info(f"Social account updated for non-frontier provider: {sociallogin.provider}")
-#         return
+    Args:
+        sender: The sender of the signal.
+        request: The HTTP request object.
+        sociallogin: The social login instance containing the account information.
+        **kwargs: Additional keyword arguments.
+    """
+    if socialaccount.provider != 'frontier':
+        return
     
-#     user:User = sociallogin.user
-#     log_dja.info(f"Social account updated for user: {user.username}")
-#     interval_schedule, create = IntervalSchedule.objects.get_or_create(
-#         every=24,  # 24 hours
-#         period=IntervalSchedule.HOURS,
-#     )
-#     defaults={
-#         'task': CapiJournalSync.name,
-#         'interval': interval_schedule,
-#         'enabled': True,
-#         'one_off': False,
-#         'queue': 'ed_dbsync',
-#         'args': [],
-#         'kwargs': str({'user_id': f'{user.id}'}).replace("'", '"'),
-#         'description': f"Sync CAPI journal for user {user.username} (ID: {user.id})",
-#     }
-#     PeriodicTask.objects.update_or_create(
-#         name=CapiJournalSync.get_task_name_for_periodic_task(user),
-#         defaults=defaults
-#     )
-#     log_dja.info(f"Periodic task for CAPI journal sync created/updated for user: {user.username} (ID: {user.id})")
-
-# @receiver(social_account_removed)
-# def remove_social_account(sender, request, socialaccount:SocialAccount, **kwargs):
-#     """
-#     Signal handler for when a social account is removed.
-#     This can be used to stop the user's CAPI journal sync task.
-
-#     Args:
-#         sender: The sender of the signal.
-#         request: The HTTP request object.
-#         sociallogin: The social login instance containing the account information.
-#         **kwargs: Additional keyword arguments.
-#     """
-#     if socialaccount.provider != 'frontier':
-#         return
-    
-#     user:User = socialaccount.user
-#     log_dja.info(f"Social account removed for user: {user.username}")
-#     try:
-#         tasck = PeriodicTask.objects.get(name=CapiJournalSync.get_task_name_for_periodic_task(user))
-#         tasck.delete()
-#         log_dja.info(f"Periodic task for CAPI journal sync removed for user: {user.username} (ID: {user.id})")
-#     except PeriodicTask.DoesNotExist:
-#         log_dja.warning(f"No periodic task found for user: {user.username} (ID: {user.id})")
+    user:User = socialaccount.user
+    log_dja.info(f"Social account removed for user: {user.username}")
+    try:
+        tasck = PeriodicTask.objects.get(name=CapiJournalSync.get_task_name_for_periodic_task(user))
+        tasck.delete()
+        log_dja.info(f"Periodic task for CAPI journal sync removed for user: {user.username} (ID: {user.id})")
+    except PeriodicTask.DoesNotExist:
+        log_dja.warning(f"No periodic task found for user: {user.username} (ID: {user.id})")
--- eddai_EliteDangerousApiInterface/users/signals.py
+++ eddai_EliteDangerousApiInterface/users/signals.py
@@ -7,41 +7,25 @@ from .models import User
 import logging
 
 log_dja = logging.getLogger("django")
-log_name = logging.getLogger(__name__)
 
 @receiver(social_account_added)
-def update_cmdr_profile(sender, **kwargs):
-    log_dja.info("DEBUG: social_account_added signal received (ed_dbsync.signals)")
-    log_name.info("DEBUG: social_account_added signal received (ed_dbsync.signals)")
-
-@receiver(social_account_updated)
-def update_social_account(sender, **kwargs):
-    log_dja.info("DEBUG: social_account_updated signal received (ed_dbsync.signals)")
-    log_name.info("DEBUG: social_account_updated signal received (ed_dbsync.signals)")
-
-@receiver(social_account_removed)
-def remove_social_account(sender, **kwargs):
-    log_dja.info("DEBUG: social_account_removed signal received (ed_dbsync.signals)")
-    log_name.info("DEBUG: social_account_removed signal received (ed_dbsync.signals)")
-
-# @receiver(social_account_added)
-# def update_cmdr_profile(sender, request, sociallogin:SocialLogin, **kwargs):
-#     """
-#     Aggiorna il profilo CMDR quando un account social viene aggiornato
-#     """
-#     log_dja.info(f"Social account updated for user: {sociallogin.user.username} (ID: {sociallogin.user.id}). Provider {sociallogin.provider} ", exc_info={"sociallogin": sociallogin})
-#     print(f"Social account updated for user: {sociallogin.user.username} (ID: {sociallogin.user.id}). Provider {sociallogin.provider} ")
-#     try:
-#         if sociallogin.provider != 'frontier':
-#             log_dja.info(f"Social account updated for non-frontier provider: {sociallogin.provider}")
-#             return
+def update_cmdr_profile(sender, request, sociallogin:SocialLogin, **kwargs):
+    """
+    Aggiorna il profilo CMDR quando un account social viene aggiornato
+    """
+    log_dja.info(f"Social account updated for user: {sociallogin.user.username} (ID: {sociallogin.user.id}). Provider {sociallogin.provider} ", exc_info={"sociallogin": sociallogin})
+    print(f"Social account updated for user: {sociallogin.user.username} (ID: {sociallogin.user.id}). Provider {sociallogin.provider} ")
+    try:
+        if sociallogin.provider != 'frontier':
+            log_dja.info(f"Social account updated for non-frontier provider: {sociallogin.provider}")
+            return
         
-#         user:User = sociallogin.user
+        user:User = sociallogin.user
 
-#         username = get_cmdr_name(user)
+        username = get_cmdr_name(user)
 
-#         if username and username != user.username:
-#             user.username = username
-#             user.save()
-#     except Exception as e:
-#         log_dja.error(f"Error updating CMDR profile for user {user.id}", exc_info=e)
+        if username and username != user.username:
+            user.username = username
+            user.save()
+    except Exception as e:
+        log_dja.error(f"Error updating CMDR profile for user {user.id}", exc_info=e)
