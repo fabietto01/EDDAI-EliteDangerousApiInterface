7a7e37aa7daebfd281a01d3d73bcb29f1b486049
fabio zorzetto <fabio.zorzetto.01@gmail.com>
2025-12-18 15:07:29 +0000

    Ottimizzato il queryset nella StationViewSet per migliorare le performance e aggiunta la configurazione della debug toolbar in dev.py e urls.py.

--- eddai_EliteDangerousApiInterface/eddai_EliteDangerousApiInterface/settings/dev.py
+++ eddai_EliteDangerousApiInterface/eddai_EliteDangerousApiInterface/settings/dev.py
@@ -1,1 +1,21 @@
 from .default import *
+
+INSTALLED_APPS = [
+    *INSTALLED_APPS,
+    "debug_toolbar",
+]
+
+MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    *MIDDLEWARE,
+]
+
+# Configurazione per dev container
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+# Configurazione debug toolbar per risolvere problemi JSON
+DEBUG_TOOLBAR_CONFIG = {
+    "RENDER_PANELS": True,
+}
