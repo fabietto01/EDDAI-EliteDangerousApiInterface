50bb56b346e0f1a886763afa9e5fd6d9f348b180
fabio zorzetto <fabio.zorzetto.01@gmail.com>
2026-01-12 17:16:30 +0000

    fix(ed_dbsync): rimosso init=False da IncomingData.guid per compatibilità Celery

--- eddai_EliteDangerousApiInterface/ed_dbsync/dataclass.py
+++ eddai_EliteDangerousApiInterface/ed_dbsync/dataclass.py
@@ -5,7 +5,7 @@ import uuid
 class IncomingData:
 
     guid: str = field(
-        init=False, compare=True,
+        compare=True,
         repr=False, metadata={"description": "Unique identifier for the incoming data"},
         default_factory=lambda: str(uuid.uuid4())
     )
@@ -18,8 +18,10 @@ class IncomingData:
         metadata={"description": "Data payload of the incoming message"}
     )
 
+    _souce_valid = ["eddn", "capi_api"]
+
     def __post_init__(self):
-        if self.source not in ["eddn", "capi_api"]:
+        if self.source not in self._souce_valid:
             raise ValueError(f"Invalid source '{self.source}'. Valid sources are: {self._souce_valid}")
         
     def to_dict(self) -> dict:
